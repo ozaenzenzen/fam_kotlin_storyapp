58be81045d101db56e1e846c3b1c37cdd356c231
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-06-26 13:52:42 +0700

    update

---
+++ app/src/main/java/com/example/famstoryappkotlin/ui/AddStoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.famstoryappkotlin.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.famstoryappkotlin.R
+
+class AddStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_story)
+    }
+}
---
+++ app/src/main/java/com/example/famstoryappkotlin/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.famstoryappkotlin.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.famstoryappkotlin.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
