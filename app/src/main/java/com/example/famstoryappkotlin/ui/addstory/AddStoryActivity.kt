52ca15d293d746ff4a47b13bef673b2571698084
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-06-27 19:10:33 +0700

    update home and add story page

--- app/src/main/java/com/example/famstoryappkotlin/ui/addstory/AddStoryActivity.kt
+++ app/src/main/java/com/example/famstoryappkotlin/ui/addstory/AddStoryActivity.kt
@@ -3,10 +3,30 @@ package com.example.famstoryappkotlin.ui.addstory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.famstoryappkotlin.R
+import com.example.famstoryappkotlin.databinding.ActivityAddStoryBinding
 
 class AddStoryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddStoryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_story)
+
+        binding = ActivityAddStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setToolbar("Create Story")
+    }
+
+    private fun setToolbar(title: String) {
+        setSupportActionBar(binding.toolbar.toolbar)
+        supportActionBar?.apply {
+            setDisplayShowHomeEnabled(true)
+            setDisplayHomeAsUpEnabled(true)
+            this.title = title
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- app/src/main/java/com/example/famstoryappkotlin/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/famstoryappkotlin/ui/main/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.famstoryappkotlin.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.famstoryappkotlin.R
+import com.example.famstoryappkotlin.databinding.ActivityMainBinding
+import com.example.famstoryappkotlin.ui.addstory.AddStoryActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar.toolbar)
+
+        binding.fabCreateStory.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddStoryActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
