9a87951f5d9d0f09404aa75f3a2cf6dcc9c8e048
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-07-02 17:55:19 +0700

    update user data preferences instance

--- app/src/main/java/com/example/famstoryappkotlin/data/local/preferences/UserDataPreferences.kt
+++ app/src/main/java/com/example/famstoryappkotlin/data/local/preferences/UserDataPreferences.kt
@@ -1,12 +1,15 @@
 package com.example.famstoryappkotlin.data.local.preferences
 
+import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "userdata")
 class UserDataPreferences private constructor(private val dataStore: DataStore<Preferences>) {
     private val TOKEN_KEY = stringPreferencesKey("data_token")
 
@@ -19,6 +22,19 @@ class UserDataPreferences private constructor(private val dataStore: DataStore<P
     suspend fun saveAuthenticationToken(token: String) {
         dataStore.edit { preferences ->
             preferences[TOKEN_KEY] = token
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: UserDataPreferences? = null
+
+        fun getInstance(dataStore: DataStore<Preferences>): UserDataPreferences {
+            return INSTANCE ?: synchronized(this) {
+                val instance = UserDataPreferences(dataStore)
+                INSTANCE = instance
+                instance
+            }
         }
     }
 }
