7524833696a8e2d03de95911a9ef54ef7de597f0
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-07-02 13:54:39 +0700

    update: user data preference func name

--- app/src/main/java/com/example/famstoryappkotlin/data/local/preferences/UserDataPreferences.kt
+++ app/src/main/java/com/example/famstoryappkotlin/data/local/preferences/UserDataPreferences.kt
@@ -10,13 +10,13 @@ import kotlinx.coroutines.flow.map
 class UserDataPreferences private constructor(private val dataStore: DataStore<Preferences>) {
     private val TOKEN_KEY = stringPreferencesKey("data_token")
 
-    fun getThemeSetting(): Flow<String?> {
+    fun getAuthenticationToken(): Flow<String?> {
         return dataStore.data.map { preferences ->
             preferences[TOKEN_KEY]
         }
     }
 
-    suspend fun saveThemeSetting(token: String) {
+    suspend fun saveAuthenticationToken(token: String) {
         dataStore.edit { preferences ->
             preferences[TOKEN_KEY] = token
         }
