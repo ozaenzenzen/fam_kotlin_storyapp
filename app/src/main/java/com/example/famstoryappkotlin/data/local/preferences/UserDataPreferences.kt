bea26c0b15d5eab6ee7b42a37ec33e8d424d8568
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-07-02 07:57:07 +0700

    update: add user data preferences

---
+++ app/src/main/java/com/example/famstoryappkotlin/data/local/preferences/UserDataPreferences.kt
@@ -0,0 +1,24 @@
+package com.example.famstoryappkotlin.data.local.preferences
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class UserDataPreferences private constructor(private val dataStore: DataStore<Preferences>) {
+    private val TOKEN_KEY = stringPreferencesKey("data_token")
+
+    fun getThemeSetting(): Flow<String?> {
+        return dataStore.data.map { preferences ->
+            preferences[TOKEN_KEY]
+        }
+    }
+
+    suspend fun saveThemeSetting(token: String) {
+        dataStore.edit { preferences ->
+            preferences[TOKEN_KEY] = token
+        }
+    }
+}
